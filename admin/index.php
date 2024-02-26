0bd32efda2ac0920c58cc94b104df7a81c024984
moustafa <samir@nuwas.net>
2024-02-27 01:36:50 +0200

    add data to database and call it back at submit

--- admin/index.php
+++ admin/index.php
@@ -5,8 +5,27 @@
 
 <?php //echo lang('MESSAGE').' '.lang('ADMIN'); ?>
 
+<?php  
 
-<form class="login text-center">
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+    $hashedPass = sha1($password);
+
+    // echo $username . ' - '.$hashedPass;
+
+    $stmt = $con->prepare("SELECT Username, Password FROM users WHERE Username = ? AND Password = ? AND GroupID = 1");
+    $stmt->execute(array($username,$hashedPass));
+    $count = $stmt->rowCount();
+    if($count > 0){
+        echo 'welcome '.$username;
+    }
+}
+
+?>
+
+<form class="login text-center" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 <h4>Admin Login</h4>
 <input class="form-control" type="text" name="user" placeholder="Username" autocomplete="off">
 <input class="form-control" type="password" name="pass" placeholder="Password" autocomplete="new-password">
